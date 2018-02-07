3e3ef2542dd6eeabccc1cda6a9a39445a1d99be9
oki2a24 <oki2a24@gmail.com>
2018-02-08 06:31:59 +0900

    セル内最後の改行を除去する

--- flattened_table.py
+++ flattened_table.py
@@ -5,8 +5,9 @@ cb = pyperclip.paste()
 # セル 1 つでリストの 1 要素とするためにタブまたは改行で区切る
 cb_splited = re.split('\t|\r\n', cb)
 # エクセル等のセル内で改行した場合、最初と最後に半角ダブルクォーテーションがあり、これを除去
+# その前に、最後に改行がある場合があるので、改行を除いてからセル内最後のダブルクォーテーションを除去
 cb_splited_striped = [
-    v.strip('"') for v in cb_splited
+    v.strip().strip('"') for v in cb_splited
 ]
 cb_splited_striped_text = '\r\n'.join(cb_splited_striped)
 print('クリップボードは以下の状態に編集されます。')
