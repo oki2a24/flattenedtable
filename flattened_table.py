b897a27e2ae9c84164667be99cfd0998ff8db155
oki2a24 <oki2a24@gmail.com>
2018-02-08 06:28:16 +0900

    タブまたは改行でリストに格納する

--- flattened_table.py
+++ flattened_table.py
@@ -1,7 +1,9 @@
 import pyperclip
+import re
 
 cb = pyperclip.paste()
-cb_splited = cb.split('\t')
+# セル 1 つでリストの 1 要素とするためにタブまたは改行で区切る
+cb_splited = re.split('\t|\r\n', cb)
 # エクセル等のセル内で改行した場合、最初と最後に半角ダブルクォーテーションがあり、これを除去
 cb_splited_striped = [
     v.strip('"') for v in cb_splited
